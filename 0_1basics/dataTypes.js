7e21cb6aab833f341e2997f6ce14002f065b515d
Suraj Pandey <surajpandey230522@gmail.com>
2026-08-14 12:44:00 +0000

    add the datavariable in basics folder

---
+++ 0_1basics/dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";// treat all JS code as newer verson
+
+//alert(3+3)// we are using node.js,not browser
+
+console.log(3+3)
+// code readability should be high
+console.log("suraj")
+
+let a = "adarsh"
+let b=123;
+let c =1.2;
+let d=true;
+let isLoggedIn=false;
+let state=null;
+
+// Number =>2 to pwer 53  
+//bignt =>use for large number
+//string=>""
+//boolean=> true or false
+//null=> standlone value
+// undifined => 
+//symbol=> unique
+//object
+
+console.log(typeof null)//object
+console.log(typeof d)
+console.log(typeof undefined)//undefined
