cc30d4620e03bf956875e1d0f74037638bfd9326
Suraj Pandey <surajpandey230522@gmail.com>
2026-08-14 11:47:36 +0000

    add a variable file and generate many types of variables

---
+++ 0_1basics/variable.js
@@ -0,0 +1,18 @@
+const accountId=1223445
+let  accountEmail ="suraj@google.com"
+var accountPassword="123456789"
+accountCity="varansi"
+let accountState;
+//accountId=2;
+accountEmail="adarsh@google.com"
+accountPassword="23123"
+accountCity="delhi"
+
+/*
+Prefer not to use var
+because of issue in block scope and fuctional scope
+
+Here scope mean that:- {}
+
+*/
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
