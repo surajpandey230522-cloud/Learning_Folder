42648334f238afbbec26246316698b22249eb1d1
Suraj Pandey <surajpandey230522@gmail.com>
2026-08-15 11:39:01 +0000

    add conversionOperation file and do work

---
+++ 0_1basics/conversionOperation.js
@@ -0,0 +1,35 @@
+let score = "34";
+
+//console.log(typeof(score))
+//console.log(typeof score)
+
+
+let numberId=Number(score)
+//console.log(typeof numberId)
+//console.log(numberId)
+
+
+//"24231" =>  24231.
+//"231abc"=> NaN
+// true=> convert in 1
+// false=>  convert in 0
+
+
+let isLoggedIn = "123numbber";
+
+let BooleanIsloggedIn=Boolean(isLoggedIn)
+//console.log(BooleanIsloggedIn)
+console.table([typeof(score),typeof numberId,numberId,BooleanIsloggedIn ])
+
+// 1 => true
+// 0 => flase
+// ""=> false
+// "surajpandey"=true
+
+let sum =33;
+let sumId=String(sum)
+//console.log(sumId)
+//console.log(typeof sumId)
+console.table([sumId,typeof sumId])
+
+
