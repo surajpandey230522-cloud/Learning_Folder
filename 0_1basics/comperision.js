d6638430c6f188d932d47b6bc1d801349d4c9c52
Suraj Pandey <surajpandey230522@gmail.com>
2026-08-15 13:02:24 +0000

    add comperision mathod

---
+++ 0_1basics/comperision.js
@@ -0,0 +1,13 @@
+
+//avoid conversion to use
+ 
+// console.log("2">1)
+// console.log("02">=2)
+// console.log("sum>1"<2)
+
+// console.log(2>1)
+
+console.log(null>0)
+console.log(null==!0)
+console.log(null==0)
+console.log(null>=0)
