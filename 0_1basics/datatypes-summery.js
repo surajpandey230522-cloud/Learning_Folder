ca076d317d3c86166077c30cefd913edec178432
Suraj Pandey <surajpandey230522@gmail.com>
2026-08-16 18:08:30 +0000

    add some variables

---
+++ 0_1basics/datatypes-summery.js
@@ -0,0 +1,25 @@
+//Data Type) Primitive : Number,String,Boolean,Null,Symbol,BigInt,undefined
+//Referance(Non-primitive):Array,Function,Object
+
+const score=100
+const anotherscore=100.1
+
+const tempreture=null
+const userEmail=0;
+
+const Id=Symbol("123")
+const anotherId=Symbol("123")
+
+console.table(Id===anotherId)
+
+console.log(Id)
+console.log(anotherId)
+
+let BigNumber=3475873245734597398n
+console.log(BigNumber)
+console.log(typeof BigNumber)
+
+
+let box=["suraj","Adarsh","utsav"]
+
+let dataId={}
