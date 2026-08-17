cb17fc36d7506781c9aabaa2144ebdeb8f27d518
Suraj Pandey <surajpandey230522@gmail.com>
2026-08-17 12:51:56 +0000

    stack and heap memory usase

--- 0_1basics/datatypes-summery.js
+++ 0_1basics/datatypes-summery.js
@@ -1,38 +1,63 @@
 //Data Type) Primitive : Number,String,Boolean,Null,Symbol,BigInt,undefined
 
 
-const score=100
-const anotherscore=100.1
+// const score=100
+// const anotherscore=100.1
 
-const tempreture=null
-const userEmail=0;
+// const tempreture=null
+// const userEmail=0;
 
-const Id=Symbol("123")
-const anotherId=Symbol("123")
+// const Id=Symbol("123")
+// const anotherId=Symbol("123")
 
-console.table(Id===anotherId)
+// console.table(Id===anotherId)
 
-console.log(Id)
-console.log(anotherId)
+// console.log(Id)
+// console.log(anotherId)
 
-let BigNumber=3475873245734597398n
-console.log(BigNumber)
-console.log(typeof BigNumber)
+// let BigNumber=3475873245734597398n
+// console.log(BigNumber)
+// console.log(typeof BigNumber)
 
 //Referance(Non-primitive):Array,Function,Object
 
-let box=["suraj","Adarsh","utsav"]
+// let box=["suraj","Adarsh","utsav"]
 
-let dataId={
-    name:"suraj",
-    rollno:2,
-    mark:2
+// let dataId={
+//     name:"suraj",
+//     rollno:2,
+//     mark:2
+// }
+
+// function myId(){
+//     console.log("ashoka")
+// }
+// myId();
+
+// console.log(dataId)
+// console.log(box)
+
+//...........................................................................................
+
+//stack memory(primitive)or Heap memory(non-primitive)
+
+
+
+let data="surajpnadey"
+let stack=data
+stack="Adarsh Pathak"
+
+console.log(stack)
+
+
+let userOne={
+    email:"suraj@google.com",
+    upi:'suraj@ybl'
 }
 
-function myId(){
-    console.log("ashoka")
-}
-myId();
+let userTwo=userOne
 
-console.log(dataId)
-console.log(box)
+userTwo.email="adarsh@google.com"
+
+console.log(userOne)
+console.log(userTwo.email)
