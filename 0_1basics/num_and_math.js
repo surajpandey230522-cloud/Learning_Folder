c399bc9e99667679435ff983fc3eb056c9166a2e
Suraj Pandey <surajpandey230522@gmail.com>
2026-08-19 19:10:50 +0000

    add raandom math mathod

---
+++ 0_1basics/num_and_math.js
@@ -0,0 +1,19 @@
+const score=400
+//console.log(score)
+
+
+let balance=new Number(10000.364)
+   console.log(balance)
+
+let gta=balance.toString()
+   console.log(gta)
+   console.log(balance.toFixed(2))
+
+const another_balance=23.995
+
+   console.log(another_balance.toPrecision(2));
+
+const num1=1000000;
+console.log(num1.toLocaleString("en-IN")) //must be importent for get psot money;
+
+
