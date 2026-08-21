c76cad432091e962eec2a26c723d82676c818e6e
Suraj Pandey <surajpandey230522@gmail.com>
2026-08-21 11:52:41 +0000

    add math mathods

--- 0_1basics/num_and_math.js
+++ 0_1basics/num_and_math.js
@@ -16,4 +16,14 @@ const another_balance=23.995
 const num1=1000000;
 console.log(num1.toLocaleString("en-IN")) //must be importent for get psot money;
 
+//........................................math..........................................
+
+console.log(Math)
+
+console.log(Math.abs(-4))   // use to only change negative to positive value
+console.log(Math.round(4.56))   // use to show of exact value
+console.log(Math.ceil(4.59))
+console.log(Math.floor(4.9))
+console.log(Math.max(2,4,5,7,2,32))  
+console.log(Math.min(1,34,5,6,7,0))
 
