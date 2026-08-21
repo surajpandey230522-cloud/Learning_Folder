7fccad4a80a00536c00245dc3fa05da340568854
Suraj Pandey <surajpandey230522@gmail.com>
2026-08-21 12:12:24 +0000

    add ramdom nums

--- 0_1basics/num_and_math.js
+++ 0_1basics/num_and_math.js
@@ -3,27 +3,40 @@ const score=400
 
 
 let balance=new Number(10000.364)
-   console.log(balance)
+   //console.log(balance)
 
 let gta=balance.toString()
-   console.log(gta)
-   console.log(balance.toFixed(2))
+   //console.log(gta)
+   //console.log(balance.toFixed(2))
 
 const another_balance=23.995
 
-   console.log(another_balance.toPrecision(2));
+   //console.log(another_balance.toPrecision(2));
 
 const num1=1000000;
-console.log(num1.toLocaleString("en-IN")) //must be importent for get psot money;
+//console.log(num1.toLocaleString("en-IN")) //must be importent for get psot money;
 
 //........................................math..........................................
 
-console.log(Math)
+//console.log(Math)
 
-console.log(Math.abs(-4))   // use to only change negative to positive value
-console.log(Math.round(4.56))   // use to show of exact value
-console.log(Math.ceil(4.59))
-console.log(Math.floor(4.9))
-console.log(Math.max(2,4,5,7,2,32))  
-console.log(Math.min(1,34,5,6,7,0))
+// console.log(Math.abs(-4))   // use to only change negative to positive value
+ //console.log(Math.round(4.51))   // use to show of exact value and dpend upon what is my value 
+// console.log(Math.ceil(4.59))
+// console.log(Math.floor(4.9))
+// console.log(Math.max(2,4,5,7,2,32))  
+// console.log(Math.min(1,34,5,6,7,0))
 
+//...........................................
+
+console.log(Math.random())
+
+console.log((Math.random()*10) + 1)
+
+console.log(Math.floor(Math.random()*10) + 1)
+
+const max =20;
+const min=10;
+
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
