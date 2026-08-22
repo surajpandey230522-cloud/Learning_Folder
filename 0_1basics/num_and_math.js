54fed3277eb6da0e8db32f5084701bf1f5212146
Suraj Pandey <surajpandey230522@gmail.com>
2026-08-22 08:51:47 +0000

    change output

--- 0_1basics/num_and_math.js
+++ 0_1basics/num_and_math.js
@@ -29,11 +29,11 @@ const num1=1000000;
 
 //...........................................
 
-// console.log(Math.random())
+console.log(Math.random())
 
-// console.log((Math.random()*10) + 1)
+console.log((Math.random()*10) + 1)
 
-// console.log(Math.floor(Math.random()*10) + 1)
+console.log(Math.floor(Math.random()*10) + 1)
 
 const max =20;
 const min=10;
