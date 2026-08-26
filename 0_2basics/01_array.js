d30fba60e540eb1e90abef416ec8aa31772e6128
Suraj Pandey <surajpandey230522@gmail.com>
2026-08-26 15:18:10 +0000

    add array mathods

--- 0_2basics/01_array.js
+++ 0_2basics/01_array.js
@@ -22,10 +22,29 @@ let Array=[1,2,3,4,5,6,5,4,3,2,5,67]
 
 //slice ; splice
 
-console.log("A",Array)
-const mya1=Array.slice(1,3)
-console.log(mya1)
+// console.log("A",Array)
+// const mya1=Array.slice(1,3)
+// console.log(mya1)
 
-console.log("B",Array)
-const mya2=Array.splice(1,3)
-console.log(mya2)
+// console.log("B",Array)
+// const mya2=Array.splice(1,3)
+// console.log(mya2)
+
+const myHeros=["ironman","hulk","capton"]
+const villen=["loki","thynos"]
+
+// myHeros.push(villen)
+//console.log(myHeros)// it is return value single array where that array are  store in other array 
+
+const allHeros=myHeros.concat(villen) /// it is return value in a new array 
+//console.log(allHeros)
+
+const all_new_array=[...myHeros,...villen]
+console.log(all_new_array)  //given the spred value 
+
+
+const another_array=[1,23,4,[3,4,5],56,[3,7,8,[8,7,9]]]
+
+const real_array=another_array.flat(Infinity)///it is use to diffrent types of array convert in single array
+console.log(real_array)
+
