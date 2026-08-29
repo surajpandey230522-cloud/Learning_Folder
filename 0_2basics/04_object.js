b442ac6a8004024f8b1d4f98d8ac2fc562556abe
Suraj Pandey <surajpandey230522@gmail.com>
2026-08-29 15:36:07 +0000

    add more concept

---
+++ 0_2basics/04_object.js
@@ -0,0 +1,17 @@
+const myObject={}
+
+myObject.name="suraj pandey"
+myObject.rollno=45
+myObject.num=2134
+myObject.id=5392
+
+console.log(myObject)
+
+const text={
+    name:"suraj",
+    fullname:{
+        name:"suraj pandey"
+    }
+}
+
+console.log(text)
