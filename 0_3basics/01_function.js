7c0950cccd8668857af6faf2a8291b726c18e27f
Suraj Pandey <surajpandey230522@gmail.com>
2026-09-03 05:55:28 +0000

    last commit

--- 0_3basics/01_function.js
+++ 0_3basics/01_function.js
@@ -1,17 +1,32 @@
-function myClass(){
+// function myClass(){
 
-    setTimeout(()=>{
-         console.log("my name is suraj")
-    },2000)
+//     setTimeout(()=>{
+//          console.log("my name is suraj")
+//     },2000)
   
-}
-myClass()
+// }
+// myClass()
+
+// //....................................................................
+
+// function addTonum(a,b){
+//     let result= a+b
+//     return result ;
+// }
+// const result=addTonum(2,34)
+// console.log("Result:",result)
 
 //....................................................................
 
-function addTonum(a,b){
-    let result= a+b
-    return result ;
+function loginUserName(username="suraj"){
+    // if(username===undefined){
+    //     console.log("please enter a user")
+    //     return
+    // }
+    if(!username){
+        console.log("please enter s user")
+        return
+    }
+    return `${username}....just logged in`
 }
-const result=addTonum(2,34)
-console.log("Result:",result)
+console.log(loginUserName())
