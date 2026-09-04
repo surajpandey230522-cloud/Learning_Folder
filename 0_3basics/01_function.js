40b4b239b7220532f726eadc019a15a43dc022e7
Suraj Pandey <surajpandey230522@gmail.com>
2026-09-04 09:05:30 +0000

    add mathiod of function

--- 0_3basics/01_function.js
+++ 0_3basics/01_function.js
@@ -29,4 +29,39 @@ function loginUserName(username="suraj"){
     }
     return `${username}....just logged in`
 }
-console.log(loginUserName())
+//console.log(loginUserName())
+
+//...............................................................................
+
+function calculartePrice(val1,val2,...num1){
+    return num1
+}
+console.log(calculartePrice(200,300,400,20000000))
+
+
+const myList={
+    name:"suraj",
+    id:45
+}
+
+function newList(anyObject){
+console.log(`user name is ${anyObject.name} and id is ${anyObject.id}`)
+}
+
+newList({
+    name:"suraj pandey",
+    id:45
+})
+
+//with array...............
+
+const array=[200,600,500,400,800]
+
+function introArray(newArray){
+    //return newArray[3]
+    return newArray;
+}
+
+//console.log(introArray(array))
+
+console.log(introArray([100,400,500,6000,6308,654]))
