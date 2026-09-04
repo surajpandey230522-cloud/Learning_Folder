77c46872641243a3ae03e7dd90cc606f9f7d2b27
Suraj Pandey <surajpandey230522@gmail.com>
2026-09-04 11:22:17 +0000

    change some mathods of scope

--- 0_3basics/02_scopes.js
+++ 0_3basics/02_scopes.js
@@ -17,8 +17,32 @@ if(true){
     let a=10
 const b=20
 var c=23
-console.log("inner",a)
+//console.log("inner",a)
 }
-console.log(a)
+//console.log(a)
 //console.log(b)
 //console.log(c)
+
+
+//..........................................................
+
+function store(){
+    const myName="suraj"
+
+    function shop(){
+        const name="youtube"
+        console.log(myName)
+    }
+    
+    shop()
+}
+//store()
+
+
+if(true){
+    const your = "suraj pandey"
+    if(your==="suraj pandey"){
+      const web="youtube"
+      console.log(your+web)
+    }
+}
