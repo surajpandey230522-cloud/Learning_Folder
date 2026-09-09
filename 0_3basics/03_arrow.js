969492d5fa22aaabf49d2916b20f6db2bec9b24c
Suraj Pandey <surajpandey230522@gmail.com>
2026-09-09 18:17:22 +0000

    here final summision of arrow function

--- 0_3basics/03_arrow.js
+++ 0_3basics/03_arrow.js
@@ -1,8 +1,8 @@
-// const web =()=>{
-//     const Mycart="suraj pandey"
-//    console.log(Mycart)
-// }
-// web()
+const web =()=>{
+    const Mycart="suraj pandey"
+   console.log(Mycart)
+}
+//web()
 
 
 const mySite=()=>{
@@ -11,4 +11,29 @@ const mySite=()=>{
     mySite.text="suraj pandey"
     console.log(`${text} and he's good`)
 }
-mySite()
+//mySite()
+
+
+const addTo =(num1,num2)=>{
+    return num1+num2
+}
+console.log(addTo(34,45))
+
+const head =()=>{
+let username="suraj pandey"
+console.log(this)
+}
+head()
+
+let num1=34
+let num2=32
+
+const number=(num1,num2)=> (num1+num2)
+console.log(number(23,23))
+
+const mylist=[1,2,3,3,4,65,5,6606]
+mylist.forEach(element => {
+    const name="suraj pandey"
+    console.log(mylist)
+});
+
