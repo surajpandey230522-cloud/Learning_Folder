680fef6c1cd8c538aeb8b4389e550756ed9f1baa
Suraj Pandey <surajpandey230522@gmail.com>
2026-09-09 17:15:30 +0000

    add some mathods

--- 0_3basics/03_arrow.js
+++ 0_3basics/03_arrow.js
@@ -1,5 +1,14 @@
-const web =()=>{
-    const Mycart="suraj pandey"
-   console.log(Mycart)
+// const web =()=>{
+//     const Mycart="suraj pandey"
+//    console.log(Mycart)
+// }
+// web()
+
+
+const mySite=()=>{
+    const text="suraj handsome boy"
+    console.log(`${text} and he is very importent `)
+    mySite.text="suraj pandey"
+    console.log(`${text} and he's good`)
 }
-web()
+mySite()
