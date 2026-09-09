521b447b5957c75cc771d2235bb7a446903a93aa
Suraj Pandey <surajpandey230522@gmail.com>
2026-09-09 18:47:52 +0000

    add iife mathods

--- 0_3basics/03_arrow.js
+++ 0_3basics/03_arrow.js
@@ -17,11 +17,11 @@ const mySite=()=>{
 const addTo =(num1,num2)=>{
     return num1+num2
 }
-console.log(addTo(34,45))
+//console.log(addTo(34,45))
 
 const head =()=>{
 let username="suraj pandey"
-console.log(this)
+//console.log(this)
 }
 head()
 
@@ -29,11 +29,42 @@ let num1=34
 let num2=32
 
 const number=(num1,num2)=> (num1+num2)
-console.log(number(23,23))
+//console.log(number(23,23))
 
 const mylist=[1,2,3,3,4,65,5,6606]
 mylist.forEach(element => {
     const name="suraj pandey"
-    console.log(mylist)
+    //console.log(mylist)
 });
 
+//,.................................................................
+
+//IIFE..
+(function (){
+    console.log("hellow")
+})//();
+
+
+(function (){
+    const website="flipkart"
+    function other(name){
+     console.log(name `${website}`)
+    }
+    other("welcome adarsh on")
+})();
+
+
+//...............................
+
+// (function () {
+//     let secret = "12345";
+
+//     console.log(secret);
+// })();
+
+// console.log(secret); // Error
+
+
+( (name)=>{
+    console.log(`${name} is problem solver `)
+})("suraj");
