05515368ced257267073aa02ed546aa8cc70bdac
Suraj Pandey <surajpandey230522@gmail.com>
2026-09-10 16:08:14 +0000

    add new file of control flow

---
+++ 0_4Control_flow/one.js
@@ -0,0 +1,16 @@
+//if
+let n=5
+if(n!=5){
+    console.log("executed")
+}
+else{
+    console.log("error")
+}
+
+let score=200
+if(score>100){
+    const power="website"
+    console.log(`the output ${power}`)
+}else{
+    console.log("error")
+}
