5c089430af2a33cbbac9c3d5e3e5b18d1b5a33c5
Suraj Pandey <surajpandey230522@gmail.com>
2026-09-10 16:48:26 +0000

    add loop mathods

--- 0_4Control_flow/one.js
+++ 0_4Control_flow/one.js
@@ -14,3 +14,32 @@ if(score>100){
 }else{
     console.log("error")
 }
+
+//loop..........................................................
+
+for(index=0;index<Array.length;index++){
+    const element = index
+    console.log(element)
+    if(element==5){
+        console.log("5 is the best num")
+    }
+    console.log(element)
+}
+
+for(i=0;i<=10;i++){
+    console.log(`outer loop value ${i}`)
+    for(j=0;j<=10;j++){
+        console.log(`inner loop value ${j} and inner loop ${i}`)
+        console.log(i+"*"+j+ "="+ i*j )  // these mathod use to create a table
+    }
+}
+
+//...................................
+
+for(i=0;i<=20;i++){
+    if(i==5){
+        console.log(`detected 5`)
+        continue
+    }
+    console.log(`log of i is ${i}`)
+}
