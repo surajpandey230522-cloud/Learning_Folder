c9f90804aa324c7a27b183c16f707212e4445bce
Suraj Pandey <surajpandey230522@gmail.com>
2026-09-12 09:41:40 +0000

    add compile

--- 0_4Control_flow/while.js
+++ 0_4Control_flow/while.js
@@ -1,7 +1,14 @@
-let i =0;
-while(i<=20){
-    console.log(i)
+// let i =0;
+// while(i<=20){
+//     console.log(i)
+//     i++
+// }
+
+
+let a=[1,2,3,4,5,6,7,7]
+// ya of ya in
+for(let i of a){
+    const sum=i+1
     i++
+    console.log(sum)
 }
-
-
