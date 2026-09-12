61859ab0065ed2131062e85f56266f93d850f9e1
Suraj Pandey <surajpandey230522@gmail.com>
2026-09-12 14:49:06 +0000

    add api

--- 0_5Dom/dom.js
+++ 0_5Dom/dom.js
@@ -1,4 +1,20 @@
-document.querySelector()
-document.querySelectorAll()
-document.getElementById()
-document.addEventListener()
+// document.querySelector()
+// document.querySelectorAll()
+// document.getElementById()
+// document.addEventListener()
+
+
+
+
+async function getCalculations() {
+
+    let response = await fetch(
+        "https://app.customgpt.ai/api/v1/projects "
+    );
+
+    let data = await response.json();
+
+    console.log(data);
+}
+
+getCalculations();
