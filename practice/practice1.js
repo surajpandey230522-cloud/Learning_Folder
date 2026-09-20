923e0919e85551b566d4d37002fda760d60bad7e
Suraj Pandey <surajpandey230522@gmail.com>
2026-09-20 10:20:17 +0000

    add while loop in array

--- practice/practice1.js
+++ practice/practice1.js
@@ -1,20 +1,101 @@
-let arr=[1,2,3,4,5,6,7,8]
+// let arr=[1,2,3,4,5,6,7,8]
 
-let index=4;
+// let index=4;
 
-console.log(arr[index]);
+// console.log(arr[index]);
 
-index++;
-index++;
+// index++;
+// index++;
 
-console.log(arr[index])
+// console.log(arr[index])
 
 
-let fruit=["apple","banana","orange","lichi","gwava"]
+// let fruit=["apple","banana","orange","lichi","gwava"]
 
-index=0;
+// index=0;
 
-while(index<fruit.length){
-    console.log(fruit[index])
-    index++;
+// while(index<fruit.length){
+//     console.log(fruit[index])
+//     index++;
+// }
+
+
+// let arr = ["A", "B", "C", "D"];
+
+// let index = 0;
+
+// console.log(arr[index]);
+
+// index++;
+
+// console.log(arr[index]);
+
+// index++;
+
+// console.log(arr[index]);
+
+
+
+// let arr1=["A","B","C","D"]
+
+// let index=3;
+
+// console.log(arr1[index])
+
+// index--;
+
+// console.log(arr1[index])
+
+
+
+// let index=0;
+
+// console.log(index++)
+// console.log(index)
+
+
+// let i =0;
+
+// console.log(++i)
+// console.log(i)
+
+
+
+// let Fruit=["apple","Banana","Mango"]
+
+// let i1=0;
+
+// console.log(Fruit[i1])
+
+// console.log(Fruit[i1])
+
+// console.log(Fruit[i1])
+
+
+
+//slider
+
+let slide=["img1","img2","img3","img4"]
+
+let index=0;
+
+console.log(slide[index])
+index++
+
+console.log(slide[index])
+index++
+console.log(slide[index])
+index++
+console.log(slide[index])
+
+
+//question
+
+let textBox=["book","pen","copy","desk","light"]
+
+let i = 0
+
+while(i<textBox.length){
+    console.log(textBox[i])
+    i++
 }
