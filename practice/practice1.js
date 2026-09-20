9a3b62187d2ec67024198f66291ad26c85ae5565
Suraj Pandey <surajpandey230522@gmail.com>
2026-09-20 09:46:53 +0000

    add practice

---
+++ practice/practice1.js
@@ -0,0 +1,20 @@
+let arr=[1,2,3,4,5,6,7,8]
+
+let index=4;
+
+console.log(arr[index]);
+
+index++;
+index++;
+
+console.log(arr[index])
+
+
+let fruit=["apple","banana","orange","lichi","gwava"]
+
+index=0;
+
+while(index<fruit.length){
+    console.log(fruit[index])
+    index++;
+}
