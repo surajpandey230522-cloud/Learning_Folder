a475711d37dd01346f6b88cb47a97d1ea96621f9
Suraj Pandey <surajpandey230522@gmail.com>
2026-09-26 15:35:11 +0000

    add loopping example

--- practice/practice2.js
+++ practice/practice2.js
@@ -1,24 +1,38 @@
-const promis1=new Promise((resolve,reject)=>{
-    resolve("Success!")
-});
+// const promis1=new Promise((resolve,reject)=>{
+//     resolve("Success!")
+// });
 
-promis1.then((value)=>{
-    console.log(value)
-});
+// promis1.then((value)=>{
+//     console.log(value)
+// });
 
-const promise2=new Promise((resolve,reject)=>{
-    throw new Error("that is error")
-});
+// const promise2=new Promise((resolve,reject)=>{
+//     throw new Error("that is error")
+// });
 
-promise2.catch((Error)=>{
-    console.log(Error)
-})
+// promise2.catch((Error)=>{
+//     console.log(Error)
+// })
 
 
-async function getData() {
-    let response = await fetch("https://example.com");
+// async function getData() {
+//     let response = await fetch("https://example.com");
 
-    let data = await response.json();
+//     let data = await response.json();
 
-    console.log(data);
+//     console.log(data);
+// }
+
+
+
+let arr1=[1,2,2,3,4,5]
+let arr2=[2,2,4,5,5,6]
+
+let result=[]
+
+for(let num of arr1 ){
+    if(arr2.includes(num) && !result.includes(num))
+        result.push(num)
+   
 }
+ console.log(result)
